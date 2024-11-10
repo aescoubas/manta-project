dbd67e8ebc89c31d8b9758c4d76b2deb65c383fb
Manuel Sopena Ballesteros <manuel.sopena@cscs.ch>
2024-11-10 13:33:54 +0100

    feat: SMD/HSM init

---
+++ crates/ochami-rs/src/hsm/state/components/http_client.rs
@@ -0,0 +1,481 @@
+use config::Value;
+
+use crate::error::Error;
+
+use super::type_system::{Component, ComponentArray, ComponentPostByNidQuery, ComponentPostQuery};
+
+pub fn get(
+    auth_token: &str,
+    base_url: &str,
+    root_cert: &[u8],
+    id: Option<&str>,
+    r#type: Option<&str>,
+    state: Option<&str>,
+    flag: Option<&str>,
+    role: Option<&str>,
+    subrole: Option<&str>,
+    enabled: Option<&str>,
+    software_status: Option<&str>,
+    subtype: Option<&str>,
+    arch: Option<&str>,
+    class: Option<&str>,
+    nid: Option<&str>,
+    nid_start: Option<&str>,
+    nid_end: Option<&str>,
+    partition: Option<&str>,
+    group: Option<&str>,
+    state_only: Option<bool>,
+    flag_only: Option<bool>,
+    role_only: Option<bool>,
+    nid_only: Option<&str>,
+) -> Result<ComponentArray, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = base_url.to_owned() + "/smd/hsm/v2/State/Components";
+
+    let response = client
+        .get(api_url)
+        .query(&[
+            id,
+            r#type,
+            state,
+            flag,
+            role,
+            subrole,
+            enabled,
+            software_status,
+            subtype,
+            arch,
+            class,
+            nid,
+            nid_start,
+            nid_end,
+            partition,
+            group,
+            state_only.map(|value| value.to_string()).as_deref(),
+            flag_only.map(|value| value.to_string()).as_deref(),
+            role_only.map(|value| value.to_string()).as_deref(),
+            nid_only,
+        ])
+        .bearer_auth(auth_token)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<ComponentArray>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn get_one(
+    auth_token: &str,
+    base_url: &str,
+    root_cert: &[u8],
+    id: &str,
+) -> Result<Component, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = format!("{}/{}/{}", base_url, "smd/hsm/v2/State/Components", id);
+
+    let response = client
+        .get(api_url)
+        .bearer_auth(auth_token)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<Component>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn get_by_nid(
+    auth_token: &str,
+    base_url: &str,
+    root_cert: &[u8],
+    nid: &str,
+) -> Result<Component, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = format!(
+        "{}/{}/{}",
+        base_url, "smd/hsm/v2/State/Components/ByNID/", nid
+    );
+
+    let response = client
+        .get(api_url)
+        .bearer_auth(auth_token)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<Component>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn get_query(
+    auth_token: &str,
+    base_url: &str,
+    root_cert: &[u8],
+    xname: &str,
+    r#type: Option<&str>,
+    state: Option<&str>,
+    flag: Option<&str>,
+    role: Option<&str>,
+    subrole: Option<&str>,
+    enabled: Option<&str>,
+    softwarestatus: Option<&str>,
+    subtype: Option<&str>,
+    arch: Option<&str>,
+    class: Option<&str>,
+    nid: Option<&str>,
+    nid_start: Option<&str>,
+    nid_end: Option<&str>,
+    partition: Option<&str>,
+    group: Option<&str>,
+    stateonly: Option<bool>,
+    flagonly: Option<bool>,
+    roleonly: Option<bool>,
+    nidonly: Option<&str>,
+) -> Result<ComponentArray, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = format!(
+        "{}/{}/{}",
+        base_url, "smd/hsm/v2/State/Components/Query", xname
+    );
+
+    let response = client
+        .get(api_url)
+        .query(&[
+            r#type,
+            state,
+            flag,
+            role,
+            subrole,
+            enabled,
+            softwarestatus,
+            subtype,
+            arch,
+            class,
+            nid,
+            nid_start,
+            nid_end,
+            partition,
+            group,
+            stateonly.map(|value| value.to_string()).as_deref(),
+            flagonly.map(|value| value.to_string()).as_deref(),
+            roleonly.map(|value| value.to_string()).as_deref(),
+            nidonly,
+        ])
+        .bearer_auth(auth_token)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<ComponentArray>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn post(
+    base_url: &str,
+    auth_token: &str,
+    root_cert: &[u8],
+    component: Component,
+) -> Result<Component, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = base_url.to_owned() + "/smd/hsm/v2/State/Components";
+
+    let response = client
+        .post(api_url)
+        .bearer_auth(auth_token)
+        .json(&component)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<Component>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn post_query(
+    base_url: &str,
+    auth_token: &str,
+    root_cert: &[u8],
+    component_query: ComponentPostQuery,
+) -> Result<ComponentArray, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = base_url.to_owned() + "/smd/hsm/v2/State/Components/Query";
+
+    let response = client
+        .post(api_url)
+        .bearer_auth(auth_token)
+        .json(&component_query)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<ComponentArray>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn post_by_nid_query(
+    base_url: &str,
+    auth_token: &str,
+    root_cert: &[u8],
+    component_by_nid_query: ComponentPostByNidQuery,
+) -> Result<Component, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = base_url.to_owned() + "/smd/hsm/v2/State/Components/ByNID/Query";
+
+    let response = client
+        .post(api_url)
+        .bearer_auth(auth_token)
+        .json(&component_by_nid_query)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<Component>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn put(
+    base_url: &str,
+    auth_token: &str,
+    root_cert: &[u8],
+    component: Component,
+) -> Result<(), Error> {
+    // Validation
+    if component.id.is_none() {
+        return Err(Error::Message(
+            "ERROR - component.id not defined".to_string(),
+        ));
+    }
+
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = format!(
+        "{}/{}/{}",
+        base_url,
+        "smd/hsm/v2/State/Components",
+        component.id.as_ref().unwrap()
+    );
+
+    let response = client
+        .put(api_url)
+        .bearer_auth(auth_token)
+        .json(&component)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        Ok(())
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn delete_all(base_url: &str, auth_token: &str, root_cert: &[u8]) -> Result<Value, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = base_url.to_owned() + "/smd/hsm/v2/State/Components";
+
+    let response = client
+        .delete(api_url)
+        .bearer_auth(auth_token)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<Value>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
+
+pub fn delete_one(
+    base_url: &str,
+    auth_token: &str,
+    root_cert: &[u8],
+    id: &str,
+) -> Result<Value, Error> {
+    let client_builder = reqwest::blocking::Client::builder()
+        .add_root_certificate(reqwest::Certificate::from_pem(root_cert)?);
+
+    // Build client
+    let client = if let Ok(socks5_env) = std::env::var("SOCKS5") {
+        // socks5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(socks5_env)?;
+
+        // rest client to authenticate
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
+
+    let api_url: String = format!("{}/{}/{}", base_url, "smd/hsm/v2/State/Components", id);
+
+    let response = client
+        .delete(api_url)
+        .bearer_auth(auth_token)
+        .send()
+        .map_err(|error| Error::NetError(error))?;
+
+    if response.status().is_success() {
+        response
+            .json::<Value>()
+            .map_err(|error| Error::NetError(error))
+    } else {
+        Err(Error::CsmError(response.json()?))
+    }
+}
---
+++ crates/ochami-rs/src/hsm/state/mod.rs
@@ -0,0 +1,2 @@
+pub mod components;
+pub mod node_map;
