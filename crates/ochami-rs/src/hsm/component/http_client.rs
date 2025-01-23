e68303d38cd769c0d72e2cca721571107b662d85
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-01-23 08:07:55 +0100

    feat: add functions to manage nid and xname translation

--- crates/ochami-rs/src/hsm/component/http_client.rs
+++ crates/ochami-rs/src/hsm/component/http_client.rs
@@ -19,6 +19,43 @@ pub async fn get_all(
     .await
 }
 
+pub async fn get_all_nodes(
+    base_url: &str,
+    auth_token: &str,
+    root_cert: &[u8],
+    nid_only: Option<&str>,
+) -> Result<ComponentArray, Error> {
+    get(
+        base_url,
+        auth_token,
+        root_cert,
+        None,
+        Some("Node"),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        nid_only,
+    )
+    .await
+}
+
+/// Get all components
+/// NOTE: nid is a comma separated list of NIDs like "1,2,3". Value "nid0001,nid0002,nid0003" is not
+/// valid values
 pub async fn get(
     base_url: &str,
     auth_token: &str,
@@ -59,58 +96,72 @@ pub async fn get(
         client_builder.build()?
     };
 
-    let api_url: String = format!("{}/{}", base_url, "hsm/v2/State/Components");
+    // Create query parameters
+    // NID query params
+    let mut nid_vec_query = nid.map(|nids| {
+        nids.split(",")
+            .map(|nid| ("nid", Some(nid)))
+            .collect::<Vec<(&str, Option<&str>)>>()
+    });
+
+    // All other query params
+    let mut query_params = vec![
+        ("id", id),
+        ("type", r#type),
+        ("state", state),
+        ("flag", flag),
+        ("role", role),
+        ("subrole", subrole),
+        ("enabled", enabled),
+        ("softwarestatus", software_status),
+        ("subtype", subtype),
+        ("arch", arch),
+        ("class", class),
+        ("nidstart", nid_start),
+        ("nidend", nid_end),
+        ("partition", partition),
+        ("group", group),
+        ("stateonly", state_only),
+        ("flagonly", flag_only),
+        ("roleonly", role_only),
+        ("nidonly", nid_only),
+    ];
+
+    if let Some(mut nid_vec_query) = nid_vec_query.take() {
+        query_params.append(&mut nid_vec_query);
+    }
+
+    let api_url: String = format!("{}/{}", base_url, "smd/hsm/v2/State/Components");
 
     let response = client
         .get(api_url)
-        .query(&[
-            ("id", id),
-            ("type", r#type),
-            ("state", state),
-            ("flag", flag),
-            ("role", role),
-            ("subrole", subrole),
-            ("enabled", enabled),
-            ("softwarestatus", software_status),
-            ("subtype", subtype),
-            ("arch", arch),
-            ("class", class),
-            ("nid", nid),
-            ("nidstart", nid_start),
-            ("nidend", nid_end),
-            ("partition", partition),
-            ("group", group),
-            ("stateonly", state_only),
-            ("flagonly", flag_only),
-            ("roleonly", role_only),
-            ("nidonly", nid_only),
-        ])
+        .query(&query_params)
         .bearer_auth(auth_token)
         .send()
         .await?;
 
-    if let Err(e) = response.error_for_status_ref() {
+    if !response.status().is_success() {
         match response.status() {
             reqwest::StatusCode::UNAUTHORIZED => {
                 let error_payload = response.text().await?;
-                let error = Error::RequestError {
-                    response: e,
-                    payload: error_payload,
-                };
+                let error = Error::Message(error_payload);
                 return Err(error);
+
+                // return Err(Error::Message(response.text().await?));
             }
             _ => {
-                let error_payload = response.json::<Value>().await?;
-                let error = Error::CsmError(error_payload);
+                let error_payload = response.text().await?;
+                let error = Error::Message(error_payload);
                 return Err(error);
+                // return Err(Error::CsmError(response.json::<Value>().await?));
             }
         }
     }
 
     response
-        .json()
+        .json::<ComponentArray>()
         .await
-        .map_err(|error| Error::NetError(error))
+        .map_err(|e| Error::NetError(e))
 }
 
 pub async fn get_one(
