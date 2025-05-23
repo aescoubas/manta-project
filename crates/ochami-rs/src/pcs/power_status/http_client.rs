b08593f7abf62701489e2097a5d49a23588367d1
Alexandre Escoubas <alexandre.escoubas@cscs.ch>
2025-05-23 14:47:46 +0200

    feat: power-status
    - Changes to support /node/{node}/power-status on manta-ws
    - Change the PowerStatus and PowerStatusAll structures to match API schemas

--- crates/ochami-rs/src/pcs/power_status/http_client.rs
+++ crates/ochami-rs/src/pcs/power_status/http_client.rs
@@ -1,71 +1,71 @@
-use serde_json::Value;
+use serde_json::{ Value, json };
 
 use crate::error::Error;
 
-use super::types::PowerStatus;
+use super::types::{
+ PowerStatus,
+ PowerStatusAll
+};
+use std::collections::HashMap;
 
 pub async fn get(
-  shasta_base_url: &str,
-  shasta_token: &str,
-  shasta_root_cert: &[u8],
-  xname_vec_opt: Option<&[&str]>,
-  power_state_filter_opt: Option<&str>,
-  management_state_filter_opt: Option<&str>,
-) -> Result<PowerStatus, Error> {
-  let client;
+    shasta_base_url: &str,
+    shasta_token: &str,
+    shasta_root_cert: &[u8],
+    xname_vec_opt: Option<&[&str]>,
+    power_state_filter_opt: Option<&str>,
+    management_state_filter_opt: Option<&str>,
+) -> Result<PowerStatusAll, Error> {
 
-  let client_builder = reqwest::Client::builder()
-    .add_root_certificate(reqwest::Certificate::from_pem(shasta_root_cert)?)
-    .use_rustls_tls();
+    let client_builder = reqwest::Client::builder()
+        .danger_accept_invalid_certs(true) // Disables SSL verification (use with caution)
+        .use_rustls_tls();
 
-  // Build client
-  if std::env::var("SOCKS5").is_ok() {
-    // socks5 proxy
-    log::debug!("SOCKS5 enabled");
-    let socks5proxy = reqwest::Proxy::all(std::env::var("SOCKS5").unwrap())?;
+    // Build client
+    let client = if std::env::var("SOCKS5").is_ok() {
+        // SOCKS5 proxy
+        log::debug!("SOCKS5 enabled");
+        let socks5proxy = reqwest::Proxy::all(std::env::var("SOCKS5").unwrap())?;
+        client_builder.proxy(socks5proxy).build()?
+    } else {
+        client_builder.build()?
+    };
 
-    // rest client to authenticate
-    client = client_builder.proxy(socks5proxy).build()?;
-  } else {
-    client = client_builder.build()?;
-  }
+    let api_url = format!("{}/power-control/v1/power-status", shasta_base_url);
 
-  let api_url = format!("{}/power-control/v1/power-status", shasta_base_url);
+    // Prepare the request body as JSON with xname as an array
+    let body = json!({
+        // TODO: this should probably be a struct that matches the expected schema
+        // here "xname" is an array of xnames
+        "xname": xname_vec_opt.map(|xname_vec| xname_vec.iter().map(|&x| x.to_string()).collect::<Vec<String>>()).unwrap_or_default(),
+        "powerStateFilter": power_state_filter_opt.unwrap_or(""),
+        "managementStateFilter": management_state_filter_opt.unwrap_or(""),
+    });
 
-  let xname_vec_str_opt: Option<String> =
-    xname_vec_opt.map(|xname_vec| xname_vec.join(","));
+    let response = client
+        .post(&api_url)
+        .json(&body) // Send the body as JSON
+        .bearer_auth(shasta_token)
+        .send()
+        .await
+        .map_err(|error| {
+            println!("Failed POST query: {:?}", error);
+            Error::NetError(error)
+        })?;
 
-  let response = client
-    .get(api_url)
-    .query(&[
-      ("xname", xname_vec_str_opt),
-      (
-        "powerStateFilter",
-        power_state_filter_opt.map(|value| value.to_string()),
-      ),
-      (
-        "managementStateFilter",
-        management_state_filter_opt.map(|value| value.to_string()),
-      ),
-    ])
-    .bearer_auth(shasta_token)
-    .send()
-    .await
-    .map_err(|error| Error::NetError(error))?;
-
-  if response.status().is_success() {
-    response
-      .json()
-      .await
-      .map_err(|error| Error::NetError(error))
-  } else {
-    let payload = response
-      .json::<Value>()
-      .await
-      .map_err(|error| Error::NetError(error))?;
-
-    Err(Error::OchamiError(payload))
-  }
+    if response.status().is_success() {
+        response
+            .json()
+            .await
+            .map_err(|error| Error::NetError(error))
+    } else {
+        println!("response is failure");
+        let payload = response
+            .json::<Value>()
+            .await
+            .map_err(|error| Error::NetError(error))?;
+        Err(Error::OchamiError(payload))
+    }
 }
 
 pub async fn post(
